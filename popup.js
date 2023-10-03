7a8a5e82c18ae283a1eda40be6bc3c7b0405cad0
Constantin Guay <const.g@gmail.com>
2023-10-03 13:06:18 +0200

    the email isbuilt.

--- popup.js
+++ popup.js
@@ -1,6 +1,22 @@
 function saveAliases() {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        console.log(tabs)
+        let DOMAIN = "mydomain.com"
+
+        let tab = tabs[0];
+        const url = new URL(tab.url);
+        hostname = url.hostname
+                        .replace(/(\.[^.]*)?$/mg, '')
+                        .replace(/^(.*\.)?/mg, '');
+
+        newAlias = 'c-' + hostname
+        + Math.floor(Math.random() * 9)
+        + Math.floor(Math.random() * 9)
+        + Math.floor(Math.random() * 9)
+        + Math.floor(Math.random() * 9)
+        + Math.floor(Math.random() * 9);
+        let email = newAlias + "@" + DOMAIN;
+
+        document.getElementById('address').textContent = email;
     });
 }
 
