0afa52e129d653b869752565694d6e6d8c4a9c25
Mickael PAPAMICA Asseline <mickael@papamica.com>
2023-10-04 01:23:09 +0200

    🔧 fix(popup.js): fix missing async/await in getAlias function to properly retrieve aliases from Infomaniak provider 🔧 fix(popup.js): fix missing declaration of aliases variable to properly display existing aliases in the popup

--- popup.js
+++ popup.js
@@ -1,5 +1,11 @@
+// Import the necessary functions from the providers folder
+import { listAliases, renameAlias } from './providers/infomaniak.js';
+
+
 let USER_PROVIDER = ""
 let USER_DOMAIN = ""
+let USER_USER = ""
+let USER_MAIL = ""
 let USER_APIKEY = ""
 
 // const { provider, mail, apikey } = chrome.storage.sync.get(
@@ -29,8 +35,10 @@ const restoreOptions = () => {
         { provider: 'not set', mail: 'not set', apikey: 'not set' },
         (items) => {
             USER_PROVIDER = items.provider;
+            USER_USER = items.mail.split('@')[0];
             USER_DOMAIN = items.mail.split('@')[1];
             USER_APIKEY = items.apikey;
+            USER_MAIL = items.mail
             document.getElementById('provider').textContent = USER_PROVIDER;
             document.getElementById('mail').textContent = items.mail;
             chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -49,6 +57,17 @@ const restoreOptions = () => {
                 let email = newAlias + "@" + USER_DOMAIN;
 
                 document.getElementById('address').textContent = email;
+                
+                
+                if (USER_PROVIDER === 'infomaniak') {
+                    async function getAlias() {
+                        const [MAILBOXS_ID, ALIASES] = await listAliases(USER_APIKEY, USER_USER, USER_DOMAIN);
+                        return ALIASES;
+                    }
+                    const aliases = getAlias()
+                }
+
+                document.getElementById('existingAliases').textContent = aliases;
             });
         }
     );
