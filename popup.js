ecd746da8468ed76540041cf43a495770638a5ef
Mickael PAPAMICA Asseline <mickael@papamica.com>
2023-10-04 03:05:19 +0200

    🔧 chore(popup.html): add script tag for infomaniak.js to enable usage of infomaniak provider 🔧 chore(popup.js): remove unnecessary importScripts statement for infomaniak.js 🔧 chore(popup.js): update function call from infomaniak.listAliases to listAliases to improve code readability and maintainability

--- popup.js
+++ popup.js
@@ -1,5 +1,3 @@
-importScripts('./providers/infomaniak.js');
-
 let USER_PROVIDER = ""
 let USER_DOMAIN = ""
 let USER_USER = ""
@@ -59,7 +57,7 @@ const restoreOptions = () => {
                 
                 if (USER_PROVIDER === 'infomaniak') {
                     async function getAlias() {
-                        const [MAILBOXS_ID, ALIASES] = await infomaniak.listAliases(USER_APIKEY, USER_USER, USER_DOMAIN);
+                        const [MAILBOXS_ID, ALIASES] = await listAliases(USER_APIKEY, USER_USER, USER_DOMAIN);
                         return ALIASES;
                     }
                     const aliases = getAlias()
